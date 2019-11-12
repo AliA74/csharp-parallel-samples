b9f199924167ffa159ba84415f89dc8084588ccc
prince_of_persia <ali74afrougheh@gmail.com>
2019-11-12 21:40:25 +0330

    First Sample Added

---
+++ Sample_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Sample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintB);
+            thread.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("A");
+            }
+        }
+
+        static void PrintB()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("B");
+            }
+        }
+    }
+}
